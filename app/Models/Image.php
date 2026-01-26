1eb9525ce4d2b353c32740902664912b9eab9bd0
lakroune <lakroune00@gmail.com>
2026-01-26 15:10:04 +0100

    Add table property to Image and Ingredient models; establish belongsToMany relationship with Recette

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
-    //
+    protected $table = 'images';
+    public function recettes()
+    {
+        return $this->belongsToMany(Recette::class);
+    }
 }
--- app/Models/Ingredient.php
+++ app/Models/Ingredient.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ingredient extends Model
 {
+    protected $table = 'ingredients';
     public function recettes()
     {
         return $this->belongsToMany(Recette::class);
