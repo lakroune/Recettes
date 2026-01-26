2a02935a860179a52a457d72ad554c8b08ace79c
lakroune <lakroune00@gmail.com>
2026-01-26 15:07:44 +0100

    Add relationships to Recette model for favoris, commentaires, images, etapes, user, and ingredients

--- app/Models/Recette.php
+++ app/Models/Recette.php
@@ -12,5 +12,31 @@ class Recette extends Model
     {
         return $this->belongsTo(Categorie::class);
     }
-    
+
+    public function favoris()
+    {
+        return $this->hasMany(Favori::class);
+    }
+    public function commentaires()
+    {
+        return $this->hasMany(Commentaire::class);
+    }
+    public function images()
+    {
+        return $this->hasMany(Image::class);
+    }
+    public function etapes()
+    {
+        return $this->hasMany(Etape::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function ingredients()
+    {
+        return $this->hasMany(Ingredient::class);
+    }
+
 }
