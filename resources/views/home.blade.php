f1a8b6806d01d45434a1744d80b0329ec995a72b
lakroune <lakroune00@gmail.com>
2026-01-30 15:10:12 +0100

    Add dynamic recipe of the day section to home view: include title, description, and image display for enhanced user engagement, along with updates to recipe preparation time formatting.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -45,6 +45,40 @@
     <x-header />
 
     <main class="max-w-7xl mx-auto px-6">
+        @isset($recette_jour)
+            <section class="py-12 animate-in">
+                <div class="grid grid-cols-1 md:grid-cols-12 gap-8 items-center">
+                    <div class="md:col-span-5 space-y-4">
+                        <span
+                            class="text-[10px] font-bold tracking-[0.2em] text-orange-600 uppercase italic underline decoration-2 underline-offset-4">Recette
+                            de jour .</span>
+                        <p class="text-gray-500 text-xs leading-relaxed max-w-xs"> Aujourd'hui, nous vous proposons une
+                            recette speciale. nous souhaitons vous offrir une experience culinaire uniques.</p>
+                        <h1 class="text-4xl md:text-5xl font-extrabold tracking-tighter leading-none">
+                            {{ $recette_jour->title_recette }}
+                            <br>Premium Edition.
+                        </h1>
+
+                        <div class="flex gap-4 pt-2">
+
+
+                            <button onclick="window.location.href='/recettes/1'"
+                                class="bg-black text-white text-[10px] font-bold uppercase tracking-wider px-6 py-3  btn-animate hover:bg-orange-600 ">Découvrir</button>
+                        </div>
+                    </div>
+                    <div class="md:col-span-7">
+                        <div class="relative group">
+                            <div
+                                class="absolute -inset-2 bg-orange-100 rounded-[2rem] -z-10 group-hover:bg-orange-200 transition duration-500">
+                            </div>
+                            <img src="{{ asset('storage/' . $recette_jour->images[0]->url_image) }}"
+                                class="w-full h-[450px] object-cover rounded-[1.5rem] shadow-2xl transition duration-500 group-hover:scale-[1.01]"
+                                alt="Hero">
+                        </div>
+                    </div>
+                </div>
+            </section>
+        @endisset
 
         <section
             class="mt-12 py-8 border-y border-gray-100 flex flex-wrap justify-around items-center gap-8 animate-in">
@@ -89,18 +123,21 @@
                     <div class="space-y-2 px-1">
                         <div
                             class="flex justify-between items-center text-[10px] font-bold text-gray-400 uppercase tracking-widest">
-                            <span>Pâtes • {{ $recette->temps_preparations }}</span>
+                            <span>Pâtes • {{ $recette->temp_preparation }}  MIN</span>
                             <span class="text-black"><i class="fa-solid fa-star text-orange-500 mr-1"></i>4.9</span>
                         </div>
                         <h3
                             class="text-xl font-extrabold tracking-tight hover:text-orange-600 transition cursor-pointer">
-                            Pasta Al Limone Premium</h3>
+                        <a href=" {{ route('recettes.show', $recette->id) }}">
+                            {{ $recette->title_recette }}</a>    
+                        </h3>
                         <div class="flex justify-between items-center pt-3 border-t border-gray-50">
                             <div class="flex items-center gap-2">
                                 <div
                                     class="w-6 h-6 rounded-full bg-black flex items-center justify-center text-[10px] text-white font-bold">
                                     A</div>
-                                <span class="text-[10px] font-bold uppercase text-gray-500">{{ $recette->user->nom . " " . $recette->user->prenom }}</span>
+                                <span
+                                    class="text-[10px] font-bold uppercase text-gray-500">{{ $recette->user->nom . ' ' . $recette->user->prenom }}</span>
                             </div>
                             <div class="flex gap-3 text-gray-300">
                                 <span class="text-[10px] font-bold text-gray-400"><i
